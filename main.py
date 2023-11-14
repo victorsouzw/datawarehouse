efd1dbabff5d5f32427055d176423980238e9bd5
Roger <rogerbambinetti@hotmail.com>
2023-11-13 21:58:37 -0300

    remove file comments

--- main.py
+++ main.py
@@ -37,19 +37,18 @@ if not input_path:
 else:
     csv_path = input_path
 print(csv_path)
+
 ##extract
 df = pd.read_csv(csv_path, delimiter=";", names=column_names, low_memory=False, header=None,
                  skiprows=1)
-##Nós já alteramos os dados no método read_csv
-## `Primeiramente, mudamos o delimitador para utilizar ponto e vírgula, ao invés de virgulas
-## e depois fazemos algumas alterações referente ao nome das colunas, onde passamos a variável "names",
-## no qual removemos caractéres especiais e espaços em branco
 
 dim_sh4 = df.copy(deep=True)[['codigo_sh4', 'descricao_sh4']].drop_duplicates()
 dim_sh2 = df.copy(deep=True)[['codigo_sh2', 'descricao_sh2']].drop_duplicates()
-dim_local = df.copy(deep=True).drop_duplicates()
 dim_secao = df.copy(deep=True)[['codigo_secao', 'descricao_secao']].drop_duplicates()
+
+dim_local = df.copy(deep=True).drop_duplicates()
 dim_calendario = df.copy(deep=True)[['ano', 'mes']].drop_duplicates()
+
 ## o que fazer com o pais? tem q criar um id e depois passar pra fato
 #fact = df[["codigo_secao","codigo_sh2", "codigo_sh4", "data", "ID DO PAIS",
  #          "NOME DA ORIGEM","valor_fob_dolar","quilograma_liquido"]]
@@ -76,15 +75,14 @@ def my_to_sql(dim, table_name):
             ##necessario para lidar com o caso de inserir um registro com primary key ja existente
             pass
 
-#my_to_sql(dim_sh4, "dim_sh4")
-#my_to_sql(dim_sh2, "dim_sh2")
-#my_to_sql(dim_secao, "dim_secao")
+#Persiste os dados em suas respectivas tabelas
+my_to_sql(dim_sh4, "dim_sh4")
+my_to_sql(dim_sh2, "dim_sh2")
+my_to_sql(dim_secao, "dim_secao")
 
 ##entender a dimensao calendario: no CSV so temos ano e mês, na tabela aparecem diversas outras
 ##talvez subir como ano e mes e alterar no BI
-my_to_sql(dim_calendario, "dim_calendario")
-
-
+#my_to_sql(dim_calendario, "dim_calendario")
 
 # check whether connection is Successful or not
 if (conn):
